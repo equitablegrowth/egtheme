897f8bbfe9d614135731cc1a4570e0baf8fade42
Austin Clemens <austinc@mac.com>
2015-06-15 16:20:11 -0400

    0.1

--- R/eg_theme.r
+++ R/eg_theme.r
@@ -7,13 +7,13 @@ theme_nyloncalc <- function(base_size = 12,base_family="") {
         strip.text =            element_text(size = rel(0.8)),
 
         axis.line =             element_blank(),
-        axis.text.x =           element_text(size=12,colour="black",face="bold",family="Lato"),
-        axis.text.y =           element_text(size=12,colour="black",face="bold",family="Lato"),
+        axis.text.x =           element_text(size=12,colour="black",family="Lato-Bla"),
+        axis.text.y =           element_text(size=12,colour="black",family="Lato-Bla"),
         axis.ticks =            element_blank(),
-        axis.title.x =          element_text(size=12,colour="black",face="bold",family="Lato",vjust=-.2),
-        axis.title.y =          element_text(size=12,colour="black",face="bold",family="Lato",angle=90,vjust=0.8),
+        axis.title.x =          element_text(size=12,colour="black",face="bold",family="Lato-Reg",vjust=-.2),
+        axis.title.y =          element_text(size=12,colour="black",face="bold",family="Lato-Reg",angle=90,vjust=0.8),
         axis.ticks.length =     unit(0.15, "cm"),
-        axis.ticks.margin =     unit(0.1, "cm"),asdf
+        axis.ticks.margin =     unit(0.1, "cm"),
 
         legend.background =     element_rect(fill="#Fbfbfb", size=0), 
         legend.margin =         unit(-.2, "cm"),
