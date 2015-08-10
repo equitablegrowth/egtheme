b57784a4dca2734fd5aa07c60bf86be38a80114e
Austin Clemens <austinc@mac.com>
2015-08-10 10:57:34 -0400

    Fix to rect

--- R/eg_theme.r
+++ R/eg_theme.r
@@ -1,7 +1,7 @@
 theme_eg <- function(base_size = 12,base_family="") {
   theme(
         line =                  element_line(colour = "black", size = 0.5, linetype = 1,lineend = "butt"),
-        rect =                  element_rect(fill = "white", colour = "black", size = 0.5, linetype = 1),
+        rect =                  element_rect(fill = "white", colour = "#eae7e5", size = 0.5, linetype = 1),
         text =                  element_text(family = base_family, face = "plain",colour = "black", size = base_size,hjust = 0.5, vjust = 0.5, angle = 0, lineheight = 0.9),
         axis.text =             element_text(size = rel(0.8), colour = "black"),
         strip.text =            element_text(size = rel(0.8)),
